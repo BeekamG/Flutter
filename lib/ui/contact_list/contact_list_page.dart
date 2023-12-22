ab94178282bfd65d58b75ce5f82a03c02c40a760
BeekamG <bekam27@astu.edu.et>
2023-12-22 04:50:13 -0800

    Custom Widget

--- lib/ui/contact_list/contact_list_page.dart
+++ lib/ui/contact_list/contact_list_page.dart
@@ -40,35 +40,9 @@ class _ContactsListPageState extends State<ContactsListPage> {
         itemCount: _contacts.length,
         //Importing and using 3rd library
         //Runs & builds every single list item
+
         itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(_contacts[index].name),
-            subtitle: Text(_contacts[index].email),
-            trailing: IconButton(
-              icon: Icon(
-                _contacts[index].isFavorite ? Icons.star : Icons.star_border,
-                color: _contacts[index].isFavorite ? Colors.amber : Colors.grey,
-              ),
-              onPressed: () {
-                setState(() {
-                  _contacts[index].isFavorite = !_contacts[index].isFavorite;
-                  //Takes in higher order function which gets passed two contacts
-                  _contacts.sort((a, b) {
-                    if (a.isFavorite) {
-                      //ContactOne will be BEFORE ContactTwo
-                      return -1;
-                    } else if (b.isFavorite) {
-                      //ContactOne will be AFTER ContactTwo
-                      return 1;
-                    } else {
-                      //The position doesn't change
-                      return 0;
-                    }
-                  });
-                });
-              },
-            ),
-          );
+          return ContactTile(contacts: _contacts);
         },
       ),
     );
